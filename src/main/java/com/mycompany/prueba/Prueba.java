fd7f47ce32b621b38c29d59c98ddcd47cd80dadd
nastking <jcastanedas10@miumg.edu.gt>
2024-03-10 15:09:12 -0600

    Jean commit2

--- src/main/java/com/mycompany/prueba/Prueba.java
+++ src/main/java/com/mycompany/prueba/Prueba.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.prueba;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ianto
@@ -11,6 +13,7 @@ package com.mycompany.prueba;
 public class Prueba {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         System.out.println("Hello World!");
         System.out.println("Prueba 1");
         System.out.println("Prueba 2");
@@ -21,9 +24,16 @@ public class Prueba {
         
         
         System.out.println("Prueba holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+        System.out.println("inserte un numero");
+
         
-        int num = 1;
-        int num1 = 2;
+        int num = entrada.nextInt() ;
+        System.out.println(" otro numero para la suma inserte un numero");
+
+
+        int num1 = entrada.nextInt();
+
         
         int suma = num + num1;
         System.err.println("La suma es: " + suma);
