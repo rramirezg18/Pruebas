2153c84f726a9aaa894eabd5da4ca427ad64b2c9
rramirezg18 <rramirezg18@miumg.edu.gt>
2024-03-10 12:11:53 -0600

    first commit

---
+++ src/main/java/com/mycompany/prueba/Prueba.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author ianto
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Prueba 1");
+    }
+}
