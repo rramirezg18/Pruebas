51b5f66740d00a53e8d245762ea4b93afef87ffb
nastking <jcastanedas10@miumg.edu.gt>
2024-03-10 14:42:39 -0600

    Commit Jean

--- src/main/java/com/mycompany/prueba/Prueba.java
+++ src/main/java/com/mycompany/prueba/Prueba.java
@@ -15,5 +15,8 @@ public class Prueba {
         System.out.println("Prueba 1");
         System.out.println("Prueba 2");
         System.out.println("Prueba 3");
+        
+        
+        System.out.println("HOLAAAAAAA!!!!!! :D");
     }
 }
